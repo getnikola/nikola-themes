e0ec5e7df808b546b44ea62559e702aa2288fa54
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-03-25 11:01:37 -0300

    themes from master

---
+++ themes/default/messages/messages_fr.py
@@ -0,0 +1,22 @@
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
+
+MESSAGES = {
+    "Also available in": "Disponible aussi en",
+    "Archive": "Archives",
+    "LANGUAGE": "Français",
+    "More posts about": "Plus de billets sur",
+    "Newer posts": "Billets récents",
+    "Next post": "Billet suivant",
+    "Older posts": "Anciens billets",
+    "Original site": "Site d'origine",
+    "Posted": "Publié",
+    "Posts about %s": "Billets sur %s",
+    "Posts for year %s": "Billets de l'année %s",
+    "Previous post": "Billet précédent",
+    "Read in English": "Lire en français",
+    "Read more": "Lire la suite",
+    "Source": "Source",
+    "Tags": "Étiquettes",
+    "old posts page %d": "anciens billets page %d",
+}
---
+++ themes/default/messages/messages_pl.py
@@ -0,0 +1,22 @@
+# -*- encoding:utf-8 -*-
+from __future__ import unicode_literals
+
+MESSAGES = {
+    "Also available in": "Również dostępny w",
+    "Archive": "Archiwum",
+    "LANGUAGE": "polski",
+    "More posts about": "Więcej postów o",
+    "Newer posts": "Nowsze posty",
+    "Next post": "Następny post",
+    "Older posts": "Starsze posty",
+    "Original site": "Oryginalna strona",
+    "Posted": "Opublikowany",
+    "Posts about %s": "Posty o %s",
+    "Posts for year %s": "Posty z roku %s",
+    "Previous post": "Poprzedni post",
+    "Read in English": "Czytaj po polsku",
+    "Read more": "Czytaj więcej",
+    "Source": "Źródło",
+    "Tags": "Tags",
+    "old posts page %d": "stare posty, strona %d",
+}
