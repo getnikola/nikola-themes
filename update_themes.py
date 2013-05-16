9fd22c1c4edf4b361750331302a07cdb8dd7d044
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-05-16 14:23:55 -0300

    handle new themes better

--- update_themes.py
+++ update_themes.py
@@ -25,85 +25,91 @@ swatches = read_data("bootswatch_themes.txt")
 themes = read_data("themes.txt")
 b_themes = read_data("bootstrap_themes.txt")
 
-## Get the themes from the master branch, and put them in themes
+# Get the themes from the master branch, and put them in themes
 
-#src = os.path.join('nikola-master', 'nikola', 'data', 'themes')
-#for root, dirs, files in os.walk(src):
-    #dst_path = os.path.join('themes', os.path.relpath(root, src))
-    #if not os.path.isdir(dst_path):
-        #os.makedirs(dst_path)
-        #for fname in files:
-            #fdst_path = os.path.join(dst_path, fname)
-            #shutil.copy2(os.path.join(root, fname), fdst_path)
+src = os.path.join('nikola-master', 'nikola', 'data', 'themes')
+for root, dirs, files in os.walk(src):
+    dst_path = os.path.join('themes', os.path.relpath(root, src))
+    if not os.path.isdir(dst_path):
+        os.makedirs(dst_path)
+        for fname in files:
+            fdst_path = os.path.join(dst_path, fname)
+            shutil.copy2(os.path.join(root, fname), fdst_path)
 
-## Create bootswatch-derived themes for bootstrap themes
-#for swatch in swatches:
-    #url = '/'.join(('http://bootswatch.com', swatch, 'bootstrap.min.css'))
-    #min_bs = requests.get(url).text
+# Create bootswatch-derived themes for bootstrap themes
+for swatch in swatches:
+    url = '/'.join(('http://bootswatch.com', swatch, 'bootstrap.min.css'))
+    min_bs = requests.get(url).text
 
-    #url = '/'.join(('http://bootswatch.com', swatch, 'bootstrap.css'))
-    #bs = requests.get(url).text
+    url = '/'.join(('http://bootswatch.com', swatch, 'bootstrap.css'))
+    bs = requests.get(url).text
 
-    #for parent in b_themes:
-        #name = "{0}_{1}".format(parent, swatch)
-        #if os.path.isdir(os.path.join('themes', name, 'assets', 'css')):
-            #continue  # Don't rebuild these all the time
-        #try:
-            #os.makedirs(os.path.join('themes', name, 'assets', 'css'))
-        #except:
-            #pass
-        #print "Downloading: ", url
-        #with open(os.path.join('themes', name, 'assets', 'css', 'bootstrap.min.css'),
-                    #'wb+') as output:
-            #output.write(min_bs)
-        #with open(os.path.join('themes', name, 'assets', 'css', 'bootstrap.css'),
-                    #'wb+') as output:
-            #output.write(bs)
-        #with open(os.path.join('themes', name, 'parent'), 'wb+') as output:
-            #output.write(parent)
+    for parent in b_themes:
+        name = "{0}_{1}".format(parent, swatch)
+        if os.path.isdir(os.path.join('themes', name, 'assets', 'css')):
+            continue  # Don't rebuild these all the time
+        try:
+            os.makedirs(os.path.join('themes', name, 'assets', 'css'))
+        except:
+            pass
+        print "Downloading: ", url
+        with open(os.path.join('themes', name, 'assets', 'css', 'bootstrap.min.css'),
+                    'wb+') as output:
+            output.write(min_bs)
+        with open(os.path.join('themes', name, 'assets', 'css', 'bootstrap.css'),
+                    'wb+') as output:
+            output.write(bs)
+        with open(os.path.join('themes', name, 'parent'), 'wb+') as output:
+            output.write(parent)
 
-#def setup_demo(theme):
-    #"""Create demo site with a theme."""
-    #path = os.path.join('sites', theme)
-    #print "Setting up:", path
-    #if not os.path.isdir(path):
-        #os.system("nikola init --demo {0}".format(path))
-        #os.system("sed --in-place \"s/# THEME = 'site'/THEME = '{0}'/\" {1}/conf.py".format(theme, path))
-    #shutil.rmtree(os.path.join(path, "themes", theme))
-    #shutil.copytree(os.path.join("themes",theme), os.path.join(path, "themes", theme))
-    #while os.path.isfile(os.path.join(path, 'themes', theme, 'parent')):
-        #theme = open(os.path.join(path, 'themes', theme, 'parent')).readline().strip()
-        #shutil.rmtree(os.path.join(path, "themes", theme))
-        #shutil.copytree(os.path.join("themes",theme), os.path.join(path, "themes", theme))
+def setup_demo(theme):
+    """Create demo site with a theme."""
+    path = os.path.join('sites', theme)
+    print "Setting up:", path
+    if not os.path.isdir(path):
+        os.system("nikola init --demo {0}".format(path))
+        os.system("sed --in-place \"s/# THEME = 'site'/THEME = '{0}'/\" {1}/conf.py".format(theme, path))
+    try:
+        shutil.rmtree(os.path.join(path, "themes", theme))
+    except:
+        pass
+    shutil.copytree(os.path.join("themes",theme), os.path.join(path, "themes", theme))
+    while os.path.isfile(os.path.join(path, 'themes', theme, 'parent')):
+        theme = open(os.path.join(path, 'themes', theme, 'parent')).readline().strip()
+        try:
+            shutil.rmtree(os.path.join(path, "themes", theme))
+        except:
+            pass
+        shutil.copytree(os.path.join("themes",theme), os.path.join(path, "themes", theme))
 
-#for theme in glob.glob('themes/*/'):
-    #if "site-planetoid" in theme:
-        #continue  # Not a demoable theme
-    #print "Theme:", theme
-    #setup_demo(os.path.basename(theme[:-1]))
+for theme in glob.glob('themes/*/'):
+    if "site-planetoid" in theme:
+        continue  # Not a demoable theme
+    print "Theme:", theme
+    setup_demo(os.path.basename(theme[:-1]))
 
-#@contextmanager
-#def cd(path):
-    #old_dir = os.getcwd()
-    #os.chdir(path)
-    #yield
-    #os.chdir(old_dir)
+@contextmanager
+def cd(path):
+    old_dir = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(old_dir)
 
-## Build each site
-#for site in glob.glob('sites/*/'):
-    #with cd(site):
-        #try:
-            #subprocess.check_call(["nikola", "build"])
-        #except Exception:
-            #print "Error building theme:", site
-            #continue
-        #p = subprocess.Popen(["nikola", "serve"])
-        #time.sleep(2)
-        #print "Snapshotting: ", site
-        #snap = os.path.join('..', site.split(os.sep)[-2]) + '.png'
-        #subprocess.check_call(["capty", "http://localhost:8000", snap])
-        #p.kill()
-        #time.sleep(1)
+# Build each site
+for site in glob.glob('sites/*/'):
+    with cd(site):
+        try:
+            subprocess.check_call(["nikola", "build"])
+        except Exception:
+            print "Error building theme:", site
+            continue
+        p = subprocess.Popen(["nikola", "serve"])
+        time.sleep(2)
+        print "Snapshotting: ", site
+        snap = os.path.join('..', site.split(os.sep)[-2]) + '.png'
+        subprocess.check_call(["capty", "http://localhost:8000", snap])
+        p.kill()
+        time.sleep(1)
 
 for theme in glob.glob('themes/*/'):
     theme = theme.split('/')[-2]
