32b5673d822e7b342ebcac93e41d0760406cab79
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-06-22 15:55:34 -0300

    make it compatible with install_theme

--- update_themes.py
+++ update_themes.py
@@ -104,25 +104,26 @@ def cd(path):
     yield
     os.chdir(old_dir)
 
-# Build each site
-for site in glob.glob('sites/*/'):
-    with cd(site):
-        try:
-            subprocess.check_call(["nikola", "build"])
-        except Exception:
-            print "Error building theme:", site
-            continue
-        p = subprocess.Popen(["nikola", "serve"])
-        time.sleep(2)
-        print "Snapshotting: ", site
-        snap = os.path.join('..', site.split(os.sep)[-2]) + '.png'
-        subprocess.check_call(["capty", "http://localhost:8000", snap])
-        p.kill()
-        time.sleep(1)
+## Build each site
+#for site in glob.glob('sites/*/'):
+    #with cd(site):
+        #try:
+            #subprocess.check_call(["nikola", "build"])
+        #except Exception:
+            #print "Error building theme:", site
+            #continue
+        #p = subprocess.Popen(["nikola", "serve"])
+        #time.sleep(2)
+        #print "Snapshotting: ", site
+        #snap = os.path.join('..', site.split(os.sep)[-2]) + '.png'
+        #subprocess.check_call(["capty", "http://localhost:8000", snap])
+        #p.kill()
+        #time.sleep(1)
 
-for theme in glob.glob('themes/*/'):
-    theme = theme.split('/')[-2]
-    subprocess.check_call('zip -r sites/{0}.zip themes/{0}'.format(theme), shell=True)
+with cd('themes'):
+    for theme in glob.glob('*/'):
+        theme = theme[:-1]
+        subprocess.check_call('zip -r ../sites/{0}.zip {0}'.format(theme), shell=True)
 
 metadata_bit = string.Template("""<!--
 .. title: ${theme}
