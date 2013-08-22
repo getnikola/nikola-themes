de507c8ca362740bb59a12bf0edb86a0664a658a
Roberto Alsina <ralsina@netmanagers.com.ar>
2013-08-21 22:01:15 -0300

    tests for redundant templates

--- scripts/test_themes.py
+++ scripts/test_themes.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals, print_function
 import glob
+import hashlib
 
 import colorama
 
@@ -21,8 +22,31 @@ def sanity_check(theme=None):
             sanity_check(theme)
         return
     themes = utils.get_theme_chain(theme)
+    engine = utils.get_template_engine(themes)
+
+    # Inheritance checks
+
+    # All themes must inherit from base
     if themes[-1] != 'base':
         error("theme {0} doesn't inherit from base".format(theme))
+    # All jinja themes must inherit from base-jinja
+    if engine == "jinja" and "base-jinja" not in themes:
+        error("theme {0} is jinja-based and doesn't inherit from base-jinja".format(theme))
+    # All mako themes must NOT inherit from base-jinja
+    if engine == "mako" and "base-jinja" in themes:
+        error("theme {0} is mako-based and inherits from base-jinja".format(theme))
+
+    # Detect exact template duplication in theme chain
+    for fname in glob.glob("/".join(["themes", theme, "templates", "*.tmpl"])):  # templates
+        with open(fname) as inf:
+            baseline = hashlib.sha224(inf.read()).hexdigest()
+        fname = "/".join(fname.split("/")[2:])
+        fname2 = utils.get_asset_path(fname, themes[1:])
+        with open(fname2) as inf:
+            new_hash = hashlib.sha224(inf.read()).hexdigest()
+        if baseline == new_hash:
+            error("theme {0} has a redundant template: {1} is identical to {2}".format(theme, fname, fname2))
+
 
 
 if __name__ == "__main__":
