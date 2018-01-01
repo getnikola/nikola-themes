70b11060707605a5bf3d9acec2dc397e1c71c912
Chris Warrick <kwpolska@gmail.com>
2018-01-01 00:00:00 +0000

    2018! :fireworks:
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- site/plugins/pkgindex_zip/pkgindex_zip.py
+++ site/plugins/pkgindex_zip/pkgindex_zip.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright © 2016-2017, Chris Warrick.
+# Copyright © 2016-2018, Chris Warrick.
 
 # Permission is hereby granted, free of charge, to any
 # person obtaining a copy of this software and associated
