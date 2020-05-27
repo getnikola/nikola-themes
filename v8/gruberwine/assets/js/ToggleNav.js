7bc6a1bcb8c23349223708c4204ce734533a644a
Martin McCallion <martin@devilgate.org>
2020-05-27 16:54:06 +0100

    Top navigation now working the way I want it.

--- v8/gruberwine/assets/js/ToggleNav.js
+++ v8/gruberwine/assets/js/ToggleNav.js
@@ -8,15 +8,3 @@ function toggleNav() {
     links.style.display = "block";
   }
 }
-
-
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
-
