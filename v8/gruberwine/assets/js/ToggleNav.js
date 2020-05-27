deaf158ba396d28adf5d2abc30a21b640a19babe
Martin McCallion <martin@devilgate.org>
2020-05-27 12:58:21 +0100

    Getting close to working using CSS flexbox, but committing prior to trying a different tack.

--- v8/gruberwine/assets/js/ToggleNav.js
+++ v8/gruberwine/assets/js/ToggleNav.js
@@ -1,10 +1,13 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on 
+/* Toggle between showing and hiding the navigation menu links when the user clicks on
 the hamburger menu / bar icon */
 function toggleNav() {
   let links = document.getElementById("navlinks");
+  let social = document.getElementById("socialNav");
   if (links.style.display === "block") {
     links.style.display = "none";
+    social.style.minHeight = "5vh";
   } else {
     links.style.display = "block";
+    social.style.minHeight = "32vh";
   }
 }
